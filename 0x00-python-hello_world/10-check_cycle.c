2ae3cafff7e0c0d4b615f3d82aafb03adc62d9ee
Gomolemo001 <gomolizandrea@gmail.com>
2022-12-01 17:20:57 +0000

    Task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,3 +10,17 @@ int check_cycle(linkint_t *list)
 {
 	listint_t *slow = list;
 	listint_t *fast = list;
+
+	if (!list)
+		return (0);
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+
+	return (0);
+}
